641325b4c13fe763dbb439848481f928c4c4b435
SabaManjavidze <sabamanjavidze@gmail.com>
2022-01-07 02:48:57 +0400

    changed to heroku api

--- src/components/variables.js
+++ src/components/variables.js
@@ -1,3 +1,3 @@
-export const main_url = "http://192.168.0.107:5000/api"
+export const main_url = "https://express-manga-api.herokuapp.com/api"
 export const img_url = "/mangaimage/manga-"
 export const domain = "https://ww.mangakakalot.tv"
